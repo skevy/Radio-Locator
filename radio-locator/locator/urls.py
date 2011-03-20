13234dc369f28bcc0f8dddb906d903e760ae1c3d
Adam Miskiewicz <adam.skevy@mac.com>
2011-03-20 15:34:37 -0400

    ...

--- radio-locator/locator/urls.py
+++ radio-locator/locator/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('views',
+urlpatterns = patterns('radio-locator.locator.views',
     url(r'^get_all_stations/$', "all_stations")
 )
