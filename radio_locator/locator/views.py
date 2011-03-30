28fe4fa6ec60209980de8f8f173552bff45a4c56
Adam Miskiewicz <adam.skevy@mac.com>
2011-03-30 19:03:48 -0400

    ...

--- radio_locator/locator/views.py
+++ radio_locator/locator/views.py
@@ -12,8 +12,22 @@ def all_stations(request):
     loc = Point(float(request.GET['lng']), float(request.GET['lat']))
     
     stations = {}
+    stations['high'] = []
+    stations['medium'] = []
         
-    stations['high'] = [s.serialize() for s in Station.objects.filter(local_range__contains=loc).distance(loc).order_by('distance')]
-    stations['medium'] = [s.serialize() for s in Station.objects.filter(distant_range__contains=loc).exclude(pk__in=[s['pk'] for s in stations['high']]).distance(loc).order_by('-frequency')]
+    high_stations = Station.objects.filter(local_range__contains=loc).distance(loc).order_by('distance')
+    medium_stations = Station.objects.filter(distant_range__contains=loc).exclude(pk__in=[s['pk'] for s in stations['high']]).distance(loc).order_by('-frequency')
+    
+    for s in high_stations:
+        station = s.serialize()
+        # station.update(distance=s.distance)
+        print s.distance
+        stations['high'].append(station)
+    
+    for s in medium_stations:
+        station = s.serialize()
+        # station.update(distance=s.distance)
+        print s.distance
+        stations['medium'].append(station)
     
     return HttpResponse(json.dumps(stations), mimetype="application/json")
